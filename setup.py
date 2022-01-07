15910b6665040a516efb184712ec2dc3aff3e53e
kyoungmo-yang <km.yang@apptest.ai>
2022-01-07 15:29:18 +0900

    'ff_pw_9' can not be used when making a shared object; recompile with -fPIC 이슈해결

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='h264decoder',
-    version="0.1.0",
+    version="0.1.1",
     author='Michael Welter',
     ext_modules=[CMakeExtension('h264decoder')],
     cmdclass=dict(build_ext=CMakeBuild),
